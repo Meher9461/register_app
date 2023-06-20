d6414a69111fc44045bf7f72e3cae76dd614e2cc
meher <baisameher@gmail.com>
2023-06-20 11:32:07 +0530

    updated files

--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,11 @@
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "publicsubnet1" {
   vpc_id                                      = aws_vpc.myvpc.id
-  cidr_block                                  = "10.3.1.0/24"
+  cidr_block                                  = "10.3.2.0/24"
   enable_resource_name_dns_a_record_on_launch = "true"
   map_public_ip_on_launch                     = "true"
   tags = {
-    Name = "subnet1"
+    Name = "publicsubnet1"
   }
 
 }
