f8431cbdb37981de359e781d22aa7d312ab3b6e0
meher <baisameher@gmail.com>
2023-06-26 12:34:50 +0530

    var file created

---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "vpc_cidr" {
+    default = "10.3.0.0/16"
+  
+}
+
+variable "publicsubnet_cidr" {
+    default = "10.3.2.0/24"
+  
+}
+
+variable "privatesubnet_cidr" {
+    default = "10.3.3.0/24"
+  
+}
+
+variable "region" {
+    default = "us-east-1"
+  
+}
+
+variable "bucket" {
+    default = "terrabucket"
+  
+}
+
+
+
