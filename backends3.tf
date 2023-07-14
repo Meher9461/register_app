32df4046a0878109803497770ce965cec9e128e4
meher <baisameher@gmail.com>
2023-07-14 11:34:03 +0530

    deleted all

--- backends3.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraformfstate"
-#     key    = "statefile/terraform.tfstate"
-#     region = "us-east-1"
-
-#   }
-# } 
--- s3bucket.tf
+++
@@ -1,8 +0,0 @@
-# resource "aws_s3_bucket" "newbucket" {
-#   bucket = var.bucket
-#   depends_on = [
-#     aws_vpc.myvpc
-#   ]
-
-
-# }
